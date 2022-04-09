1fdddd256784d0a2874e19d4e5979c5ab4f6069d
youwei <1690312648@qq.com>
2022-04-09 16:05:53 +0800

    注册页

---
+++ frontend/src/views/register.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="register">
+        <el-container>
+            <el-header height="40px">后台管理系统</el-header>
+            <el-main>
+                <el-form
+                    :model="registerForm"
+                    status-icon
+                    :rules="rules"
+                    ref="registerForm"
+                    class="register-form"
+                    label-width="100px"
+                >
+                    <el-form-item label="用户名：" prop="username">
+                        <el-input v-model="registerForm.username" placeholder="请输入用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱：" prop="email">
+                        <el-input v-model="registerForm.email" placeholder="请输入邮箱"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码：" prop="password">
+                        <el-input
+                            type="password"
+                            v-model="registerForm.password"
+                            placeholder="请输入密码"
+                        ></el-input>
+                    </el-form-item>
+                    <el-form-item label="确认密码：" prop="password2">
+                        <el-input
+                            type="password"
+                            v-model="registerForm.password2"
+                            placeholder="请再次输入密码"
+                        ></el-input>
+                    </el-form-item>
+                    <el-form-item label="请选择身份：">
+                        <el-select v-model="registerForm.identity" placeholder="请选择">
+                            <el-option key="0" label="管理员" value="0"></el-option>
+                            <el-option key="1" label="员工" value="1"></el-option>
+                        </el-select>
+                    </el-form-item>
+
+                    <el-button
+                        class="register-btn"
+                        type="primary"
+                        @click="submit('registerForm')"
+                    >注册</el-button>
+                </el-form>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            registerForm: {
+                username: '',
+                email: '',
+                password: '',
+                password2: '',
+                identity: '',
+            },
+            rules: {
+            }
+        };
+    },
+    methods: {
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.register {
+    background-image: url("@/assets/register-bg.webp");
+    height: 100%;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.el-container {
+    margin-top: -50px;
+    flex: none;
+    width: 400px;
+    height: 500px;
+
+    .el-main {
+        background-color: #fff;
+        border-radius: 5px;
+        padding-top: 40px;
+    }
+
+    .el-header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 20px;
+        color: #fff;
+        font-size: 30px;
+        margin-bottom: 20px;
+        font-weight: bold;
+    }
+
+    .register-btn,
+    .el-select {
+        width: 100%;
+    }
+}
+</style>
