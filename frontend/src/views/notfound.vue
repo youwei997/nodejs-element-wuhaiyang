7ebeb6b77bf9966e906107962c3d128fe24a3f34
youwei <1690312648@qq.com>
2022-04-09 15:26:34 +0800

    404页面及图片

---
+++ frontend/src/views/notfound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="not_found">
+        <img src="@/assets/not_found.webp" alt />
+    </div>
+</template>
+<style scoped>
+.not_found {
+    width: 100%;
+    height: 100%;
+}
+</style>
