b1233515e178164dc3ca8dab5940848c8d19a1a3
youwei <1690312648@qq.com>
2022-04-09 23:13:40 +0800

    增加是否登录和用户数据相关

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,14 +3,60 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// const types = {
+//   SET_AUTHENTICATED: 'SET_AUTHENTICATED',
+//   SET_USER: "SET_USER"
+// }
+
 export default new Vuex.Store({
   state: {
+    isAuthenticated: false,
+    user: {}
   },
   getters: {
+    isAuthenticated: state => state.isAuthenticated,
+    user: state => state.user,
   },
   mutations: {
+    // [types.SET_AUTHENTICATED](state, isAuthenticated) {
+    //   if (isAuthenticated) {
+    //     state.isAuthenticated = true
+    //   } else {
+    //     state.isAuthenticated = false
+    //   }
+    // },
+    // [types.SET_USER](state, user) {
+    //   if (user) {
+    //     state.user = user
+    //   } else {
+    //     state.user = {}
+    //   }
+    // }
+
+    //用解析token 判断对象是否为空，不为空就是已授权（登录）
+    SET_AUTHENTICATED(state, isAuthenticated) {
+      if (isAuthenticated) {
+        state.isAuthenticated = true
+      } else {
+        state.isAuthenticated = false
+      }
+    },
+    //解析token获取的用户数据，存入
+    SET_USER(state, user) {
+      if (user) {
+        state.user = user
+      } else {
+        state.user = {}
+      }
+    }
   },
   actions: {
+    setAuthenticated({ commit }, isAuthenticated) {
+      commit('SET_AUTHENTICATED', isAuthenticated)
+    },
+    setUser({ commit }, user) {
+      commit('SET_USER', user)
+    },
   },
   modules: {
   }
