0dcbaa4eb4e4bb0f88259e3329941969d3e0104a
youwei <1690312648@qq.com>
2022-04-09 20:55:15 +0800

    全局挂载$http

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//第三方
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
+
+//自定义
+import http from '@/utils/http';
+Vue.prototype.$http = http
 
 //重置样式css
 import '@/style/reset.css'
