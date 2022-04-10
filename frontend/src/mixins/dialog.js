5724bd7db5a6b9735db416c1aee66deedd857006
youwei <1690312648@qq.com>
2022-04-10 21:43:46 +0800

    停止使用dialog mixins

--- frontend/src/mixins/dialog.js
+++ frontend/src/mixins/dialog.js
@@ -1,17 +1,37 @@
+/* 有问题后面再封装 */
 export default {
     data() {
         return {
             dialogStatus: false,
+            operationType: '', //add是新增 edit是编辑 view是查看
         }
     },
     mounted() {
     },
     methods: {
-        dialogShow() {
+        dialogShow(type, index, row) {
+            console.log(index, row);
             this.dialogStatus = true
+            switch (type) {
+                case 'add':
+                    console.log('添加');
+                    break;
+                case 'edit':
+                    console.log('编辑');
+                    break;
+                case 'view':
+                    console.log('查看');
+            }
         },
         dialogClose() {
             this.dialogStatus = false
-        }
+        },
+        // handleEdit(index, row) {
+        //     console.log(index, row);
+        //     this.operationType = 'edit'
+        // },
+        // handleDelete(index, row) {
+        //     console.log(index, row);
+        // },
     }
 }
