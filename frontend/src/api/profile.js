ff8599cba336fdc44ce715cd58a3a0865fcdb137
youwei <1690312648@qq.com>
2022-04-10 21:44:12 +0800

    增加资金管理 增删改查api

--- frontend/src/api/profile.js
+++ frontend/src/api/profile.js
@@ -1,9 +1,44 @@
 import http from '@/utils/http';
 
+//查询列表
 export const getProfileList = (data) => {
     return http({
-        url: '/api/profiles/',
+        url: '/api/profiles',
         method: 'get',
         data
+    })
+}
+
+//查询单个
+export const getProfile = (data) => {
+    return http({
+        url: '/api/profiles/' + data,
+        method: 'get',
+    })
+}
+
+//添加
+export const addProfile = (data) => {
+    return http({
+        url: '/api/profiles/add',
+        method: 'post',
+        data
+    })
+}
+
+//更新
+export const updateProfile = (data) => {
+    return http({
+        url: '/api/profiles/edit/' + data._id,
+        method: 'post',
+        data
+    })
+}
+
+//删除
+export const deleteProfile = (data) => {
+    return http({
+        url: '/api/profiles/delete/' + data,
+        method: 'delete',
     })
 }
